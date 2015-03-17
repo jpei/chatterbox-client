1358cd4bfe6d1cb86f674c19932b7777322cff19
Joseph Pei <josephdpei@gmail.com>
2015-03-16 21:04:29 -0700

    Send is almost complete

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,7 +2,7 @@ var app = {
 	
 	lastUpdate : 0,
 
-	getMessages : function(query) {
+	fetch : function(query) {
 		$.ajax({
 		  // This is the url you should use to communicate with the parse API server.
 		  url: 'https://api.parse.com/1/classes/chatterbox',
@@ -17,15 +17,33 @@ var app = {
 		});
 	},
 
-	postMessage : function(message) {
-
+	send : function(message, username) {
+		$.ajax({
+		  // This is the url you should use to communicate with the parse API server.
+		  url: 'https://api.parse.com/1/classes/chatterbox',
+		  type: 'POST',
+		  data: function() { return username ? JSON.stringify({username:username, text:message, roomname:'lobby'}) : JSON.stringify({username:'anon', text:message, roomname:'lobby'}) }(),
+		  contentType: 'application/json',
+		  success: function (data) {
+		    console.log('chatterbox: Message sent');
+		  },
+		  error: function (data) {
+		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		    console.error('chatterbox: Failed to send message');
+		  }
+		});
 	},
 
 	init : function() {
-		//this.getMessages('$all');
-		this.getMessages('where={"createdAt":{"$gte":"2015-03-16"}}')
-		//this.getMessages('where={"roomname":"lobby"}')
-		// setInterval(this.getMessages .bind(this), 3000);
+		this.fetch('order=-createdAt');
+		$( "#form" ).submit(function( event ) {
+		  app.send($("input").val(), window.location.search.slice(10));
+		  debugger;
+		  setTimeout(fetch, 500);
+		});
+		//this.fetch('where={"createdAt":{"$gte":"2015-03-16"}}')
+		//this.fetch('where={"roomname":"lobby"}')
+		// setInterval(this.fetch .bind(this), 3000);
 	},
 
 	updatePage : function(data) {
@@ -38,10 +56,9 @@ var app = {
 		// roomname + timestamp
 
 		this.lastUpdate = new Date().getTime();
-		debugger;
 		for (var i = 0; i < data.results.length; i++) {
 			var message = data.results[i];
-			if (message.text !== undefined) {
+			if (message.text !== undefined && message.text.toLowerCase().indexOf('prompt')===-1 && message.text.toLowerCase().indexOf('alert')===-1) {
 				$('#messages').prepend('<div class="chat">' + '<span class="username">' + message.username + '</span>' + ': ' + message.text + '</div>');
 				// message.timestamp 
 			}
@@ -49,5 +66,6 @@ var app = {
 	}
 
 };
-
-app.init();
+$(document).ready(function() {
+	app.init();
+});
