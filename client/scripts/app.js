dc5da970672c1691a0ada3a442937c93f56989cd
Joseph Pei <josephdpei@gmail.com>
2015-03-17 15:32:17 -0700

    Added rooms, friend functionality, and sanitizer

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,17 @@
 var app = {
 	
-	newestTimestamp:null,
+	newestTimestamp: null,
+	currentRoom: 'lobby',
+	friends: {},
 
 	fetch : function(query) {
 		$.ajax({
-		  // This is the url you should use to communicate with the parse API server.
 		  url: 'https://api.parse.com/1/classes/chatterbox',
 		  type: 'GET',
 		  data: query,
 		  contentType: 'application/json',
-		  success: this.postMessages,
+		  success: this.displayMessages,
 		  error: function (data) {
-		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
 		    console.error('chatterbox: Failed to send message');
 		  }
 		});
@@ -21,7 +21,7 @@ var app = {
 		$.ajax({
 		  url: 'https://api.parse.com/1/classes/chatterbox',
 		  type: 'POST',
-		  data: JSON.stringify({username:username, text:message, roomname:'lobby'}),
+		  data: JSON.stringify({username:username, text:message, roomname:app.currentRoom}),
 		  contentType: 'application/json',
 		  success: function (data) {
 		    console.log('chatterbox: Message sent');
@@ -33,46 +33,55 @@ var app = {
 	},
 
 	submit : function() {
-		app.send($('input').val(), window.location.search.slice(10));
-		$('.text').val('');
-		app.fetch();
-		app.update();
+		app.send($('input.postfield').val(), window.location.search.slice(10));
+		$('.postfield').val('');
+		setTimeout(app.update.bind(app),50);
 	},
 
 	init : function() {
-		this.fetch('order=-createdAt');
-
-		//this.fetch('where={"createdAt":{"$gte":"2015-03-16"}}')
-		//this.fetch('where={"roomname":"lobby"}')
-		// setInterval(this.fetch .bind(this), 3000);
-		setInterval(this.update.bind(this),1000);
+		app.update();
+		setInterval(app.update.bind(app),1000);
+		$('body').on('click','.username',function() {
+			app.friends[$(this).text()] = !app.friends[$(this).text()];
+			var usernames = $('.username');
+			for (var i=0; i<usernames.length; i++) {
+				if ($(usernames[i]).text() === $(this).text()) {
+					$(usernames[i]).toggleClass('friend');
+				}
+			}
+		});
 	},
 
-	postMessages : function(data) {
-		// data.results[i] has properties: createdAt, objectId, roomname, text, updatedAt, username
-
-		// clear old messages
-		// post new messages
-		// username + text
-		// roomname + timestamp
-
-		this.lastUpdate = new Date().getTime();
-		if (data.results.length>0) {
-			this.newestTimestamp = data.results[0].createdAt;
-		}
-		for (var i = 0; i < data.results.length; i++) {
+	// data.results[i] has properties: createdAt, objectId, roomname, text, updatedAt, username
+	displayMessages : function(data) {
+		for (var i = data.results.length-1; i >= 0; i--) {
 			var message = data.results[i];
-			if (message.text !== undefined && message.text.toLowerCase().indexOf('prompt')===-1 && message.text.toLowerCase().indexOf('alert')===-1) {
-				$('#messages').append('<div class="chat">' + '<span class="username">' + message.username + '</span>' + ': ' + message.text + '</div>');
-				// message.timestamp 
+			if ((!app.newestTimestamp || message.createdAt > app.newestTimestamp) && message.text !== undefined) {
+				var spanClass = '<span class="username">'
+				if( app.friends[message.username] ) {
+					spanClass = '<span class="username friend">'
+				}
+				$('#messages').prepend('<div class="chat">' + spanClass + html_sanitize(message.username) + '</span>' + ': ' + html_sanitize(message.text) + '</div>');
 			}
-		};
+		}
+		if (data.results.length>0) {
+			app.newestTimestamp = data.results[0].createdAt;
+		}
 	},
 
 	update : function() {
 		if (this.newestTimestamp !== null) {
-			this.fetch('where={"createdAt":{"$gte":'+newestTimestamp+'}}')
+			this.fetch({order:'-createdAt', where:{createdAt:{"$gt":this.newestTimestamp}, roomname:app.currentRoom}});
 		}
+		else this.fetch({order:'-createdAt', where:{roomname: app.currentRoom}, limit:100})
+	},
+
+	join : function() {
+		app.currentRoom = html_sanitize($('input.roomfield').val());
+		$('.roomfield').val('');
+		app.newestTimestamp = null;
+		$('.chat').remove();
+		app.update();
 	}
 
 };
